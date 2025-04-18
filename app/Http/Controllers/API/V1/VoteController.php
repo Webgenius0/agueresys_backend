2151f8146332a809e2ca8523ecfd6f796ab21b9a
helalscript <husoaib422@gmail.com>
2025-04-18 11:08:56 +0600

    home page vote and counter vote nagitive value count1

--- app/Http/Controllers/API/V1/VoteController.php
+++ app/Http/Controllers/API/V1/VoteController.php
@@ -103,4 +103,79 @@ class VoteController extends Controller
         }
     }
 
+    public function SingleGodRoleStore(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            // 'anonymous_user_id' => 'required|exists:anonymous_users,id',
+            'god_role_id' => 'required|exists:god_roles,id',
+            'vote' => 'required|in:up,down',
+        ]);
+
+        try {
+            DB::beginTransaction();
+            // Retrieve fingerprint from request headers
+            $fingerprint = $request->header('Fingerprint');
+            $ipAddress = $request->ip();
+
+            if (!$fingerprint) {
+                return response()->json(['message' => 'Fingerprint header missing'], 400);
+            }
+
+            $fingerprint = $request->header('Fingerprint');
+            $anonymousUser = AnonymousUser::where('fingerprint', $fingerprint)->first();
+            // Check if the anonymous user exists
+            if (!$anonymousUser) {
+                AnonymousUser::create([
+                    'ip_address' => $ipAddress . 15,
+                    'fingerprint' => $fingerprint
+                ]);
+                $anonymousUser = AnonymousUser::where('fingerprint', $fingerprint)->first();
+            }
+
+            $anonymousUserId = $anonymousUser->id;
+            $godRole = GodRole::findOrFail($request->god_role_id);
+            $godId = $godRole->god_id;
+            //if exits vote
+            $vote = Vote::where('anonymous_user_id', $anonymousUserId)->where('god_role_id', $request->god_role_id)->first();
+            if ($vote) {
+                // dd($vote->toArray());
+                if ($vote->vote === $validatedData['vote']) {
+                    // dd('old vote');
+                    $vote->delete();
+                    DB::commit();
+                    return Helper::jsonErrorResponse('Your vote has been changed and deleted', 200);
+                } else {
+                    // dd('change vote');
+                    $vote->update(['vote' => $validatedData['vote']]);
+                    // Log::info('Vote updated successfully' . $vote);
+                    DB::commit();
+                    return Helper::jsonResponse(true, 'Vote successfully recorded', 200);
+                }
+            } else {
+                // Count how many roles this user already voted for this god
+                $voteCount = Vote::whereHas('godRole', function ($query) use ($godId) {
+                    $query->where('god_id', $godId);
+                })->where('anonymous_user_id', $anonymousUserId)->count();
+
+                if ($voteCount >= 2) {
+                    return Helper::jsonErrorResponse('You can only vote for 2 roles per god', 403);
+                }
+
+                // Store the vote
+                Vote::create([
+                    'anonymous_user_id' => $anonymousUserId,
+                    'god_role_id' => $request->god_role_id,
+                    'vote' => $validatedData['vote']
+                ]);
+            }
+            // Log::info('last vote' . $vote);
+
+            DB::commit();
+            return Helper::jsonResponse(true, 'Vote successfully recorded', 200);
+        } catch (Exception $e) {
+            DB::rollBack();
+            Log::error("VoteController::store" . $e->getMessage());
+            return Helper::jsonErrorResponse('Failed to record vote' . $e->getMessage(), 500);
+        }
+    }
 }
